7b91c036fc7b1ffe8d0a8d514b57efbf7f9fe4c2
Eduardo Thomazi <eduardothomazi91@gmail.com>
2020-06-06 08:06:57 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
 			System.out.print("Source: ");
 			ChessPosition source = UI.readChessPosition(sc);
 			System.out.println();
-			System.out.println("Target: ");
+			System.out.print("Target: ");
 			ChessPosition target = UI.readChessPosition(sc);
 			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 			
